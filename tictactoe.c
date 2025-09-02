c40fde7e33a63bf9f3dd15ca76b74489c54a8163
adil-km <adilkmparappur@gmail.com>
2025-09-02 21:04:44 +0530

    fix: not user freindly board

--- tictactoe.c
+++ tictactoe.c
@@ -14,13 +14,17 @@ char board[3][3] ={
                 };
 
 int playCount = 0;
-void displayBoard(){
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3;j++){
-            printf(" %c ",board[i][j]);
+void displayBoard() {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < 2) printf("|");
         }
         printf("\n");
+        if (i < 2) printf("---+---+---\n");
     }
+    printf("\n");
 }
 
 int play(char symbol,int row, int col){
